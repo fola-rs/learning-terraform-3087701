5b8f0331ba79dc9f49c582de58d5ac7a4041283d
fola-rs <120256365+fola-rs@users.noreply.github.com>
2026-08-03 09:04:04 +0100

    Update AWS AMI filters and owners in main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,19 @@
 data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["aws-marketplace"]
+ most_recent = true
+ filter {
+   name   = "name"
+   values = ["al2023-ami-*-x86_64"]
+ }
+ filter {
+   name   = "virtualization-type"
+   values = ["hvm"]
+ }
+ owners = ["amazon"]
 }
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+ ami           = data.aws_ami.app_ami.id
+ instance_type = "t3.nano"
+ tags = {
+   Name = "HelloWorld"
+ }
 }
